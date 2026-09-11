06846acfb4ff9e9b2af5eb8be2daf04957f1ac20
harshit04122008-netizen <harshit04122008@gmail.com>
2026-09-11 16:45:06 +0530

    comment lones didnt look cool

--- lists_and_tuples/problem_1/problem_1.py
+++ lists_and_tuples/problem_1/problem_1.py
@@ -1,9 +1,4 @@
 n = int(input())
-#input for the numbers of workers 
-
 L = input().split(" ")
-#input for the list of workers
-
 for i in range(n):
-    print((L[i-2]),end=" ") #to make the last two workers move to the front of the list, we use i-2 as the index to print the workers in a circular manner
-    
+    print((L[i-2]),end=" ")    
--- lists_and_tuples/problem_3/problme_3.py
+++ lists_and_tuples/problem_3/problme_3.py
@@ -1,49 +1,17 @@
 L = input().split()
-#input for the list of numbers
-
 i = 0
-#intializing the index variable to 0
-
 L_2 =  []
-#creating an empty list to store the peaks
-
-while i<(len(L)-1):     
-    #loop through the list
-    
-    if i<(len(L)-2):    
-        #sepearting because the last two elements of the list will not  have  a next element to compare with
-
+while i<(len(L)-1):
+    if i<(len(L)-2):
         a = int(L[i])
-        #first number to compare with the next two numbers
-
         b = int(L[i+1])
-        #second number to compare with the first and third numbers
-
         c = int(L[i+2])
-        #third number to compare with the first and second numbers
-
-        if b>a and b>c:    
-             #to check if the second number is greater than the first and third numbers
-
+        if b>a and b>c:
             L_2.append(b)
-            #adding the second number to the list of peaks if it is greater than the first and third numbers
-
-    elif i<len(L): 
-        #for the last two elements of the list, we will only compare with the next element
-
+    elif i<len(L):
         a = int(L[i])
-        #first number to compare with the next number
-
         b = int(L[i+1])
-        #second number to compare with the first number
-
         if b>a:
-            #to check if the second number is greater than the first number
-
             L_2.append(b)
-            #adding the second number to the list of peaks if it is greater than the first number
     i = i + 1
-        #incrementing the index variable to move to the next element in the list
-
 print("Peaks:",L_2)
-#printing the list of peaks
