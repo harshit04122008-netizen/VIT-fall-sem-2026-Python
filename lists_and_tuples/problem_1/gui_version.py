67164f9963e059c9a7036717f57b5815b45362a2
harshit04122008-netizen <harshit04122008@gmail.com>
2026-09-11 20:13:13 +0530

    just little spaces make ti look better

--- lists_and_tuples/problem_1/gui_version.py
+++ lists_and_tuples/problem_1/gui_version.py
@@ -2,15 +2,18 @@ from nicegui import ui
 
 n_input = ui.input(label='Enter a number')
 l_input = ui.input(label='Enter a list of numbers separated by spaces')
+
 result_label = ui.label()
 
 def calculate():
+
     n = int(n_input.value)
     l = [int(x) for x in l_input.value.split()]
 
     output = ""
 
     for i in range(n):
+
         output += f"{l[i-2]}"
 
     result_label.set_text(f"Result: {output}")
